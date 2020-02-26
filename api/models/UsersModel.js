5e91f90a9863e8a0843ef062ad4a2eea0e6af379
AceIsHuman <menoman1995@yahoo.com>
2020-02-26 04:52:24 -0600

    return everything on user add, update id reference

--- api/models/UsersModel.js
+++ api/models/UsersModel.js
@@ -12,11 +12,9 @@ function getAll_users() {
 }
 
 function add(user) {
-  return db("users").insert(user);
+  return db("users").insert(user, ['*']);
 }
 
-
-
-function getUserById(firebase_user_id, changes) {
-  return db("users").where({ firebase_user_id })
+function getUserById(id, changes) {
+  return db("users").where({ id })
 }
