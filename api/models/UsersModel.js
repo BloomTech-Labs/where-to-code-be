d73346682aa0f5e3bca2a645f08bc4da8abdeba6
AceIsHuman <menoman1995@yahoo.com>
2020-02-27 22:07:56 -0600

    create update helper function

--- api/models/UsersModel.js
+++ api/models/UsersModel.js
@@ -3,18 +3,22 @@ const db = require("../../config/knexConfig");
 module.exports = {
   getAll_users,
   add,
-  getUserById
-
+  getUserById,
+  update
 };
 
 function getAll_users() {
   return db("users");
-}
+};
 
 function add(user) {
   return db("users").insert(user, ['*']);
-}
+};
 
-function getUserById(id, changes) {
+function getUserById(id) {
   return db("users").where({ id })
-}
+};
+
+function update(id, changes) {
+  return db("users").where({ id }).update(changes, ['*']);
+};
