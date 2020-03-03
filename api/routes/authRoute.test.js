74b8b8d066bbece8dab86f7dbc7a2fcd04b66254
AceIsHuman <menoman1995@yahoo.com>
2020-03-02 23:56:25 -0600

    test user login

--- api/routes/authRoute.test.js
+++ api/routes/authRoute.test.js
@@ -1,20 +1,35 @@
 const server = require("../server.js");
 const request = require("supertest");
 
-describe('REGISTER USER', () => {
-  it('Should return status code 201, and contain token', async () => {
+describe("REGISTER USER", () => {
+  it("Should return status code 201, and contain token", async () => {
     const res = await request(server)
-      .post('/auth/user/register')
+      .post("/auth/user/register")
       .send({
         email: "htmlRulZd00d@gmail.com",
-        username: 'SuperHacker',
+        username: "SuperHacker",
         password: "sup3r5ecr3tkey",
         firstName: "James",
         lastName: "Bond"
       })
-      .set('Content-Type', 'application/json');
+      .set("Content-Type", "application/json");
     
     expect(res.status).toBe(201);
     expect(res.body.token).toBeTruthy();
-  })
+  });
+});
+
+describe("USER LOGIN", () => {
+  it("Should login successfully, returning a token", async () => {
+    const res = await request(server)
+      .post("/auth/login")
+      .send({
+        email: "htmlRulZd00d@gmail.com",
+        password: "sup3r5ecr3tkey"
+      })
+      .set("Content-Type", "application/json");
+    
+    expect(res.status).toBe(200);
+    expect(res.body.token).toBeTruthy();
+  });
 });
