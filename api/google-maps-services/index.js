9c949d8e26923655810a2493121a31813d13adb0
AceIsHuman <menoman1995@yahoo.com>
2020-03-31 01:15:16 -0500

    add try catch blocks for error handling

--- api/google-maps-services/index.js
+++ api/google-maps-services/index.js
@@ -5,25 +5,36 @@ const {
 
 module.exports = {
   googleLocationObject
-}
+};
 
 async function getPhotoUrl(ref) {
-  const res = await googlePlacePhoto(ref);
-  return res.request.res.responseUrl;
-};
+  try {
+    const res = await googlePlacePhoto(ref);
+    return res.request.res.responseUrl;
+  } catch (err) {
+    return;
+  }
+}
 
 async function googleLocationObject(location) {
-  const res = await googleLocation(location.googleId);
-  const loc = res.data.result;
-  const photo = await getPhotoUrl(loc.photos[0].photo_reference);
-  
-  const structuredResponse = {
-    id: location.id,
-    name: loc.name,
-    address: loc.formatted_address,
-    phone: loc.formatted_phone_number,
-    icon: photo || loc.icon
-  };
+  try {
+    const res = await googleLocation(location.googleId);
+    const loc = res.data.result;
+    if (!loc) return { message: "Invalid googleId provided." };
 
-  return structuredResponse;
-};
+    let photo;
+    if (!!loc.photos) photo = await getPhotoUrl(loc.photos[0].photo_reference);
+
+    const structuredResponse = {
+      id: location.id,
+      name: loc.name,
+      address: loc.formatted_address,
+      phone: loc.formatted_phone_number,
+      icon: photo || loc.icon
+    };
+
+    return structuredResponse;
+  } catch (err) {
+    return { message: `Error grabbing location info: ${err.message}` };
+  }
+}
