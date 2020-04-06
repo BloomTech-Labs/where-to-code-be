b44df03747cb83a6f4b0412f834da208eb51e15f
AceIsHuman <menoman1995@yahoo.com>
2020-04-05 23:58:10 -0500

    if id is not an integer, skip over

--- api/middleware/locations/findLocation.js
+++ api/middleware/locations/findLocation.js
@@ -1,6 +1,10 @@
+const LOCATIONS_MODEL = require("../../models/LocationsModel");
+
+module.exports = findLocation;
+
 /*
   When used on a route with `/:id` as a request paramater,
-    findLocation() takes the provided id and searched through
+    findLocation() takes the provided id and searches through
     both `location.id` & `location.googleId` for a match
 
   When used on a route with request body, findLocation() will use
@@ -11,33 +15,33 @@
     `res.locals.location`
 */
 
-const LOCATIONS_MODEL = require("../../models/LocationsModel");
-
-module.exports = findLocation;
-
 function findLocation(req, res, next) {
   let location = req.body;
   if (!!req.params.id)
     location = { id: req.params.id, googleId: req.params.id };
 
-  const findBy = id => {
-    if (location[id])
+  const findBy = (id) => {
+    if (location[id]) {
+      if (id === "id" && !Number.isInteger(location[id])) { // "id" is expected as an integer
+        return new Promise((resolve) => resolve([])); // IF not passed as integer do not call getLocationBy(
+      }
       return LOCATIONS_MODEL.getLocationBy({ [id]: location[id] }); // if location object has id, return getLocationBy(id)
-    return new Promise(resolve => resolve([])); // else return an empty array
+    }
+    return new Promise((resolve) => resolve([])); // else return an empty array
   };
 
-  const assign = location => {
+  const assign = (location) => {
     res.locals.location = location; // assign found location to res.locals for use in request
     return next();
   };
 
-  findBy("googleId").then(loc => {
+  findBy("googleId").then((loc) => {
     if (loc.length) assign(loc[0]);
     else
-      findBy("id").then(loc => {
+      findBy("id").then((loc) => {
         if (loc.length) assign(loc[0]);
         else
-          findBy("address").then(loc => {
+          findBy("address").then((loc) => {
             if (loc.length) assign(loc[0]);
             return next();
           });
